72113a72ecae450a086790ad46f48ea339ac1e81
VIPUL BHOIR <105812315+VIPULNARESHBHOIR@users.noreply.github.com>
2024-08-07 17:16:59 +0530

    Add files via upload

--- Plant_Disease_Detection/Testing/test.py
+++ Plant_Disease_Detection/Testing/test.py
@@ -3,8 +3,8 @@ from tensorflow.keras import models, layers
 import numpy as np
 from PIL import Image 
 
-saved_modelT1 = tf.keras.models.load_model(r'C:\Users\bhoir\Downloads\Plant_Disease_Detection\models\tomatoE5.h5',compile=False)
-#saved_modelP1 = tf.keras.models.load_model(r'models\potatoes10.h5',compile=False)
+saved_modelT1 = tf.keras.models.load_model(r'models\tomatoE5.h5',compile=False)
+saved_modelP1 = tf.keras.models.load_model(r'models\potatoes10.h5',compile=False)
 
 class Plant_DD:
 
@@ -22,27 +22,30 @@ class Plant_DD:
             'Potato___healthy']
 
 
-
-    def prediction(self, model, img):
+    def prediction(self, model, img, classes):
         self.im = img
         self.model = model 
         im_array = tf.keras.preprocessing.image.img_to_array(self.im)
         im_array = tf.expand_dims(im_array, 0)
         self.prediction = self.model.predict(im_array)
+        if classes == "tomato":
+            self.result(self.tomato_classes)
+        else:
+            self.result(self.potato_classes)
 
-    def result(self):
-        print("Predicted Label: ", self.tomato_classes[np.argmax(self.prediction)])
+    def result(self, category):
+        print("Predicted Label: ", category[np.argmax(self.prediction)])
         print("Confidence: ", np.max(self.prediction)*100 )
 
-image1 = Image.open('C:\\Users\\bhoir\\Downloads\\Plant_Disease_Detection\\Testing\\Sample images\\yellow_curl_virus_tomato.JPG') 
-image2 = Image.open(r'C:\Users\bhoir\Downloads\Plant_Disease_Detection\Testing\Sample images\Bacterial_l.JPG') 
-d1 = Plant_DD()
-d1.prediction(saved_modelT1, image1)
-d1.result()
 
+image1 = Image.open(r'Testing\Sample images\Bacterial_Tomato.JPG')
+d1 = Plant_DD()
+d1.prediction(saved_modelT1, image1, "tomato")
+
+image2 = Image.open(r'\Testing\Sample images\PotatoLate.JPG') 
 d2 = Plant_DD()
-d2.prediction(saved_modelT1, image2)
-d2.result()
+d2.prediction(saved_modelP1, image2, "potato")
+
 
 
 
