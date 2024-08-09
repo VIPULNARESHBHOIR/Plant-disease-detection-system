177638f909e2169179c9cd4bdfd70e8e724e93ac
VIPUL BHOIR <105812315+VIPULNARESHBHOIR@users.noreply.github.com>
2024-08-09 10:40:48 +0530

    Update test.py

--- Plant_Disease_Detection/Testing/test.py
+++ Plant_Disease_Detection/Testing/test.py
@@ -1,10 +1,13 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = "0"
+
 import tensorflow as tf   
 from tensorflow.keras import models, layers
 import numpy as np
-from PIL import Image 
+from PIL import Image
 
 saved_modelT1 = tf.keras.models.load_model(r'models\tomatoE5.h5',compile=False)
-saved_modelP1 = tf.keras.models.load_model(r'models\potatoes10.h5',compile=False)
+saved_modelP1 = tf.keras.models.load_model(r'models\potatoes21.h5',compile=False)
 
 class Plant_DD:
 
@@ -15,7 +18,7 @@ class Plant_DD:
         'Tomato_Spider_mites_Two_spotted_spider_mite',
         'Tomato__Tomato_YellowLeaf__Curl_Virus',
         'Tomato__Tomato_mosaic_virus',
-        'Tomato_healthy']
+        'Tomato_healthy'] # Excluded late and Early blight disease from dataset
 
         self.potato_classes = ['Potato___Early_blight', 
             'Potato___Late_blight',
@@ -35,14 +38,14 @@ class Plant_DD:
 
     def result(self, category):
         print("Predicted Label: ", category[np.argmax(self.prediction)])
-        print("Confidence: ", np.max(self.prediction)*100 )
+        print("Confidence: ", round(np.max(self.prediction)*100,2) )
 
 
-image1 = Image.open(r'Testing\Sample images\Bacterial_Tomato.JPG')
+image1 = Image.open(r'Testing\Sample images\spider.jpg')
 d1 = Plant_DD()
 d1.prediction(saved_modelT1, image1, "tomato")
 
-image2 = Image.open(r'\Testing\Sample images\PotatoLate.JPG') 
+image2 = Image.open(r'Testing\Sample images\potato-diseases.jpg')
 d2 = Plant_DD()
 d2.prediction(saved_modelP1, image2, "potato")
 
